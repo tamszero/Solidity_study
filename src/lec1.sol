3ab307f329485f2bc0fb737f514e56be16f5ceb6
JihyeHan <orenooshi@gmail.com>
2026-03-13 23:50:42 +0900

    Hello solidity 출력하기
    Added SPDX license identifier and Solidity version pragma.

--- src/lec1.sol
+++ src/lec1.sol
@@ -1,1 +1,6 @@
-//다 날렸네 쓴거 ;;
+//SPDX-License-Identifier : GPL-3.0
+pragma solidity >= 0.7.0 < 0.9.0; //솔리디티의 컴파일 버전 명시 입니다 ( 즉 0.7 ~0.9 의 버전을 사용)
+
+contract Hello{
+    string public hi = "Hello solidity";
+}
