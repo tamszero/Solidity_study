1e4efee90a807abf6e53c14785e2e3dbf045b69b
tamszero <orenooshi@gmail.com>
2026-03-24 02:30:27 +0900

    상속의 super

---
+++ src/lec15.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier:GPL-30
+pragma solidity >= 0.7.0 < 0.9.0;
+
+//상속의 super 사용하기
+
+contract Father {
+    event FatherName(string name);
+    function who() public virtual{
+        emit FatherName("KimDaeho");
+    }
+}
+
+contract Mother {
+    event MotherName(string name);
+    function who() public virtual{
+        emit MotherName("leeSol");
+    }
+}
+
+contract Son is Father{
+    event sonName(string name);
+    function who() public override{
+        super.who();
+        emit sonName("KimJin");
+    }
+}
