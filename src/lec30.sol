4c76ae60a60a62e5a458d8ede23b84b98800c238
tamszero <orenooshi@gmail.com>
2026-04-01 22:38:02 +0900

    modifier

---
+++ src/lec30.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier:GPL-30
+pragma solidity >= 0.7.0 < 0.9.0;
+
+//modifier
+//여러 함수에 한번에 적용 가능
+
+
+contract lec30{
+
+    //모디파이어의 파라미터가 없을 때
+    modifier onlyAdults{
+        revert("You are not allowed to pay for the cigarette");
+        _; //함수 실행부 -> 함수가 어디에 적용이 되는지 ;
+    }
+
+    function BuyCigarette() public onlyAdults returns(string memory){
+        return "Your payment is succeeded";
+    }
+
+    //모디파이어가 파라미터 값을 받을 때
+
+    modifier onlyAdults2(uint256 _age){
+        require(_age>18, "You are not allowed to pay the cigarette");
+        _; //함수 실행부
+
+    }
+
+    function BuyCigarette2(uint256 _age) public onlyAdults2(_age) returns (string memory){
+        return "Your payment is succeeded";
+    }
+        
+    
+    
+    uint256 public num = 5;
+    modifier numChange{
+        _;
+        num = 10;
+    }
+    function numChangeFunction() public numChange{
+        num = 15;
+    }
+
+    //num의 값은 10이 됨
+
+}
