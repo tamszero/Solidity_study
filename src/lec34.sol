37dff91198b5799fb8bd6b782d99066e140dd9ca
tamszero <orenooshi@gmail.com>
2026-04-06 21:19:22 +0900

    payable with 생성자, owner 적용

---
+++ src/lec34.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier:GPL-30
+pragma solidity >= 0.7.0 < 0.9.0;
+
+// payable을 생성자에 넣을 때
+// 특정 주소에게만 권한 주기
+
+contract MobileBanking{
+
+    //생성자 -> 컨트랙이 발행될 때 젤 처음으로 실행됨
+    //여기에 payable이 붙음 -> 컨트랙이 처음 배포될 때 이더를 받게되는 것이다
+    /*
+    constructor() payable{
+
+    }
+    */
+
+    address owner;
+    constructor() payable {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner{
+        require(msg.sender == owner, "Only Owner!");
+        _;
+    }
+
+    event SendInfo(address _msgSender, uint256 _currentValue);
+    event MyCurrentValue(address _msgSender, uint256 _value);
+    event CurrentValueOfSomeone(address _msgSender, address _to, uint256 _value);
+
+    function sendEther(address payable _to) public onlyOwner payable {
+        require(msg.sender == owner, "Only Owner!");
+        require((msg.sender).balance>=msg.value, "Your balance is not enough");
+        _to.transfer(msg.value);
+        emit SendInfo(msg.sender,(msg.sender).balance);
+    }
+
+    
+    function checkValueNow() public onlyOwner{
+        emit MyCurrentValue(msg.sender, msg.sender.balance);
+    }
+
+    function checkUserMoney(address _to) public onlyOwner{
+        emit CurrentValueOfSomeone(msg.sender, _to, _to.balance);
+    }
+}
