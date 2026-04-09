b5ebccef2a9eb159ea55ccaef660ab3738cdc677
tamszero <orenooshi@gmail.com>
2026-04-09 22:10:44 +0900

    lec40에 임포트할 파일

---
+++ src/lec41_1.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier:GPL-30
+pragma solidity >= 0.7.0 < 0.9.0;
+
+library SafeMath{
+    //0~255
+    function add(uint8 a, uint8 b) internal pure  returns(uint8){
+        require(a+b >= a, "SafeMath: addition overflow");
+        return a + b;
+    }
+}
+
+contract HiSolidity{
+    event Hi(string _str);
+
+    function hi() public {
+        emit Hi("Hello solidity");
+    }
+}
